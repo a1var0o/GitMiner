2fd88523f1992cfaefc066398b9c013b9b2c0e85
“alvaro <alvarova605@gmail.com>
2025-04-23 17:10:23 +0200

    add Project logic

---
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -0,0 +1,41 @@
+package aiss.gitminer.controller;
+
+import aiss.gitminer.exception.ProjectNotFoundException;
+import aiss.gitminer.model.Project;
+import aiss.gitminer.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/gitminer/projects")
+public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Project getProjectById(@PathVariable String id) throws ProjectNotFoundException {
+        Optional<Project> project = projectRepository.findById(id);
+        if (!project.isPresent()) {
+            throw new ProjectNotFoundException();
+        }
+        return project.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Project create(@Valid @RequestBody Project project) {
+        return projectRepository.save(project);
+    }
+
+}

4d7f84130305b17421e53ff580820eb682a22d7e
Sparky062 <victormt001@gmail.com>
2025-05-04 19:42:40 +0200

    fix IssueController

--- src/main/java/aiss/gitminer/controller/IssueController.java
+++ src/main/java/aiss/gitminer/controller/IssueController.java
@@ -1,41 +1,72 @@
 package aiss.gitminer.controller;
 
-import aiss.gitminer.exception.ProjectNotFoundException;
+import aiss.gitminer.exception.IssueNotFoundException;
+import aiss.gitminer.exception.UserNotFoundException;
+import aiss.gitminer.model.Comment;
 import aiss.gitminer.model.Issue;
-import aiss.gitminer.model.Project;
+import aiss.gitminer.model.User;
 import aiss.gitminer.repository.IssueRepository;
-import aiss.gitminer.repository.ProjectRepository;
+import aiss.gitminer.repository.UserRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @Tag(name = "Issue", description = "Issue management API")
 @RestController
-@RequestMapping("/gitminer/projects")
+@RequestMapping("/gitminer/issues")
 
 public class IssueController {
 
     @Autowired
     IssueRepository issueRepository;
-    ProjectRepository projectRepository;
+    @Autowired
+    UserRepository userRepository;
 
-    @GetMapping("/issues")
-    public List<Issue> getAllIssues() {
-        return issueRepository.findAll();
+    @GetMapping
+    public List<Issue> getAllIssues(@RequestParam (required = false) String authorId, @RequestParam (required = false) String state) throws UserNotFoundException {
+        List<Issue> issues;
+        if (authorId != null && state != null) {
+            Optional<User> user = userRepository.findById(authorId);
+            if (user.isPresent()) {
+                issues = issueRepository.findByAuthorAndState(user.get(), state);
+            } else {
+                throw new UserNotFoundException();
+            }
+        } else if (authorId != null) {
+            Optional<User> user = userRepository.findById(authorId);
+            if (user.isPresent()) {
+                issues = issueRepository.findByAuthor(user.get());
+            } else {
+                throw new UserNotFoundException();
+            }
+        } else if (state != null) {
+            issues = issueRepository.findByState(state);
+        } else {
+            issues = issueRepository.findAll();
+        }
+        return issues;
     }
 
-    @GetMapping("/{id}/issues")
-    public List<Issue> getProjectIssues(@PathVariable String id) throws ProjectNotFoundException {
-        Optional<Project> project = projectRepository.findById(id);
-        if (!project.isPresent()) {
-            throw new ProjectNotFoundException();
+    @GetMapping("{id}")
+    public Issue getIssueById(@PathVariable String id) throws IssueNotFoundException {
+        Optional<Issue> issue = issueRepository.findById(id);
+        if (issue.isPresent()) {
+            return issue.get();
+        } else {
+            throw new IssueNotFoundException();
         }
-        return project.get().getIssues();
+    }
+
+    @GetMapping("{id}/comments")
+    public List<Comment> getCommentsIssue(@PathVariable String id) throws IssueNotFoundException {
+        Optional<Issue> issue = issueRepository.findById(id);
+        if (!issue.isPresent()) {
+            throw new IssueNotFoundException();
+        } else {
+            return issue.get().getComments();
+        }
     }
 }
--- src/main/java/aiss/gitminer/repository/IssueRepository.java
+++ src/main/java/aiss/gitminer/repository/IssueRepository.java
@@ -1,7 +1,15 @@
 package aiss.gitminer.repository;
 
 import aiss.gitminer.model.Issue;
+import aiss.gitminer.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IssueRepository extends JpaRepository<Issue, String> {
+    List<Issue> findByAuthor(User user);
+    List<Issue> findByState(String state);
+    List<Issue> findByAuthorAndState(User user, String state);
 }

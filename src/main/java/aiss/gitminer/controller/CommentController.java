528fd0144cdf8f45af651911a4f92c1554430367
alonsog0 <6blazex6@gmail.com>
2025-05-04 19:48:16 +0200

    CommentController, CommentRepository and Comment exception added.

---
+++ src/main/java/aiss/gitminer/controller/CommentController.java
@@ -0,0 +1,37 @@
+package aiss.gitminer.controller;
+
+import aiss.gitminer.exception.CommentNotFoundException;
+import aiss.gitminer.model.Comment;
+import aiss.gitminer.repository.CommentRepository;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/gitminer/comments")
+public class CommentController {
+
+    @Autowired
+    CommentRepository repository;
+
+    @GetMapping
+    public List<Comment> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Comment findById(@Parameter(description = "id of the comment to be searched")
+                            @PathVariable String id) throws CommentNotFoundException {
+        Optional<Comment> comment = repository.findById(id);
+        if (!comment.isPresent()){
+            throw new CommentNotFoundException();
+        }
+        return comment.get();
+    }
+}

218df5ce714ef5829edd4e128b30b2a5cc327882
León Martín <leonmartinl@icloud.com>
2025-05-06 18:20:00 +0200

    Documentation

--- src/main/java/aiss/gitminer/controller/CommitController.java
+++ src/main/java/aiss/gitminer/controller/CommitController.java
@@ -3,7 +3,13 @@ package aiss.gitminer.controller;
 import aiss.gitminer.exception.CommitNotFoundException;
 import aiss.gitminer.model.Commit;
 import aiss.gitminer.repository.CommitRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +28,18 @@ public class CommitController {
     @Autowired
     CommitRepository repository;
 
+    @Operation(summary = "Retrieve all commits", description="Get all commits", tags={"commits", "get"})
+    @ApiResponse(responseCode = "200", description = "List of commits", content = { @Content(array = @ArraySchema(schema = @Schema(implementation = Commit.class)), mediaType="application/json") })
     @GetMapping
     public List<Commit> findAll() {
         return repository.findAll();
     }
 
+    @Operation(summary = "Retrieve a commit by its id", description="Get commit by id", tags={"commits", "get"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Commit", content = { @Content(schema = @Schema(implementation = Commit.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "Commit not found", content = { @Content(schema = @Schema())})
+    })
     @GetMapping("/{id}")
     public Commit findById(@Parameter(description = "id of commit to be searched")
                                @PathVariable String id) throws CommitNotFoundException {
